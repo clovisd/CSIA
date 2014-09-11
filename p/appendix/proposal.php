202cf1ce33a5de51b3261d15d564e21e91dc9896
clovisd <clovisd@cloviss-mbp.delmotte-net>
2014-09-11 20:07:58 +0200

    Update

---
+++ p/appendix/proposal.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Computer Science: Clovis Delmotte</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Computer Science Internal Assessment</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="http://CS.CLOVISD.COM/">Home</a></li>
+            <li><a href="/p/plan">Plan</a></li>
+            <li><a href="/p/record">Record</a></li>
+            <li><a href="/p/design">Design</a></li>
+            <li><a href="/p/development">Development</a></li>
+            <li><a href="/p/video">Video</a></li>
+            <li><a href="/p/evaluation">Evaluation</a></li>
+            <li class="active"><a href="/p/appendix">Appendix</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+<h2 align=center style='text-align:center'><span>&nbsp &nbsp &nbsp</span></h2>
+<h2 align=center style='text-align:center'><span>&nbsp &nbsp &nbsp</span></h2>
+<div style="text-align: center;">
+	<div class="panel panel-primary">
+  		<div class="panel-heading">
+   			<h3 class="panel-title">Proposal Document. [Appendix]</h3>
+  		</div>
+  	<div class="panel-body">
+ 		<iframe align=center style='text-align:center' src="https://docs.google.com/document/d/1-9o-X35XqcClCCJ1SUlsBiuZ-QSoKO2wVfTwT54-9zw/pub?embedded=true" style="border:0px #FFFFFF none;" name="Plan" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="800px" width="100%"></iframe>
+ 	</div>
+ 	<div class="panel-footer" align=center style='text-align:center'>
+		<div class="btn-group" align=center style='text-align:center'>
+			<a href="proposal.pdf" id="loading-3" class="btn btn-primary" role="button" data-loading-text="Downloading!" align=center style='text-align:center'>Download PDF</a>
+			<a href="proposal.docx" id="loading-2" class="btn btn-success" role="button" data-loading-text="Downloading!" align=center style='text-align:center'>Download DOC</a>
+			<a href="proposal.txt" id="loading-1" class="btn btn-warning" role="button" data-loading-text="Downloading!" align=center style='text-align:center'>Download TXT</a>
+		</div>
+	</div>
+</div>
+	
+</div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
