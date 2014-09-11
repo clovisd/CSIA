fb2fd28d242043f33b4e32f19c80638d4eed31d4
clovisd <clovisd@cloviss-mbp.delmotte-net>
2014-09-11 19:24:42 +0200

    Initial Upload of Files

---
+++ index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Computer Science: Clovis Delmotte</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">s
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Computer Science Internal Assessment</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="/p/plan">Plan</a></li>
+            <li><a href="/p/record">Record</a></li>
+            <li><a href="/p/design">Design</a></li>
+            <li><a href="/p/development">Development</a></li>
+            <li><a href="/p/video">Video</a></li>
+            <li><a href="/p/evaluation">Evaluation</a></li>
+            <li><a href="/p/appendix">Appendix</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+<br>
+<br>
+<div class="page-header">
+  <h1>Computer Science IA<small><small> Main Information</small></small></h1>
+</div>
+<table>
+<tr>
+<td width=320 colspan=2><p><b>Candidate name: </b><span class="label label-default">Clovis Delmotte</span></p></td>
+<td width=400><p><b>School number:&nbsp &nbsp &nbsp</b><span class="label label-default">International School of Geneva</span></p></td>
+<td width=200></td>
+<tr>
+
+<tr>
+<td width=320 colspan=2><p><b>Solution title: </b><span class="label label-default"> History Resources Database</span></p></td>
+<td width=400><p><b>Candidate session number:&nbsp &nbsp &nbsp</b><span class="label label-default">001</span></p></td>
+<td width=200></td>
+<tr>
+
+<tr height = '30'>
+<td width=320 colspan=2></p></td>
+<td width=400></td>
+<td width=200></td>
+<tr>
+
+<tr>
+<td width=320 colspan=2><p><b>User name: </b><span class="label label-default">clovisd</span></p></td>
+<td width=400></td>
+<td width=200></td>
+<tr>
+
+
+
+<tr>
+<td width=320 height=50 colspan=2><p><b>Links to:</b> Download (v1)</p></td>
+<td width=400></td>
+<td width=200></td>
+<tr>
+</table>
+
+<div class="page-header">
+  <h1>Criteria Core Files<small><small> Details and Downloads</small></small></h1>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Plan/#222222:#ffffff" alt="Planning Image">
+      <div class="caption">
+        <h3>Planning <small><small><span class="label label-success">Version 2.0 </span> <span class="label label-success">Submitted</span></small></small></h3>
+        <div><span class="label label-warning">Criterion A</span> <span class="label label-primary">Due: September 2nd 2014</span></div><br>
+		<div class="btn-group ">
+		  <a href="/p/plan" class="btn btn-primary" role="button">View</a>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="/p/plan/plan.pdf">PDF</a></li>
+    			<li><a href="/p/plan/plan.docx">Word Doc</a></li>
+        		<li><a href="/p/plan/plan.txt">Text File</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Solution Overview/#222222:#ffffff" alt="Solution Overview">
+      <div class="caption">
+        <h3>Solution Overview <small><small><span class="label label-danger">Version: N/A</span> <span class="label label-danger">On-Going</span></small></small></h3>
+        <div><span class="label label-warning">Criterion B</span> <span class="label label-primary">Due: September 30th 2014</span></div><br>
+		<div class="btn-group ">
+		  <button type="button" class="btn btn-primary">View</button>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="#">PDF</a></li>
+    			<li><a href="#">Word Doc</a></li>
+        		<li><a href="#">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Development/#222222:#ffffff" alt="Development">
+      <div class="caption">
+        <h3>Development <small><small><span class="label label-danger">Version: N/A</span> <span class="label label-danger">On-Going</span></small></small></h3>
+        <div><span class="label label-warning">Criterion C</span> <span class="label label-primary">Due: November 25th 2014</span></div><br>
+		<div class="btn-group ">
+		  <button type="button" class="btn btn-primary">View</button>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="#">PDF</a></li>
+    			<li><a href="#">Word Doc</a></li>
+        		<li><a href="#">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Functionality/#222222:#ffffff" alt="Functionality">
+      <div class="caption">
+        <h3>Functionality <small><small><span class="label label-danger">Version: N/A</span> <span class="label label-danger">On-Going</span></small></small></h3>
+        <div><span class="label label-warning">Criterion D</span> <span class="label label-primary">Due: December 16th 2014</span></div><br>
+		<div class="btn-group ">
+		  <button type="button" class="btn btn-primary">View</button>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="#">PDF</a></li>
+    			<li><a href="#">Word Doc</a></li>
+        		<li><a href="#">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Evaluation/#222222:#ffffff" alt="Evaluation">
+      <div class="caption">
+        <h3>Evaluation <small><small><span class="label label-danger">Version: N/A</span> <span class="label label-danger">On-Going</span></small></small></h3>
+        <div><span class="label label-warning">Criterion E</span> <span class="label label-primary">Due: January 8th 2015</span></div><br>
+		<div class="btn-group ">
+		  <button type="button" class="btn btn-primary">View</button>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="#">PDF</a></li>
+    			<li><a href="#">Word Doc</a></li>
+        		<li><a href="#">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="page-header">
+  <h1>Appendix<small><small> Additional Files and References</small></small></h1>
+</div>
+<div>
+<div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Proposal/#222222:#ffffff" alt="Interview">
+      <div class="caption">
+        <h3>Interview <small><small><span class="label label-success">Version: Final v2</span> <span class="label label-success">Submitted</span></small></small></h3>
+        <div><span class="label label-warning">Appendix</span> <span class="label label-primary">Due: Summer 2014</span></div><br>
+		<div class="btn-group ">
+		  <a href="/p/appendix/proposal.php" class="btn btn-primary" role="button">View</a>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="/p/appendix/proposal.pdf">PDF</a></li>
+    			<li><a href="/p/appendix/proposal.docx">Word Doc</a></li>
+        		<li><a href="/p/appendix/proposal.txt">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Interview/#222222:#ffffff" alt="Interview">
+      <div class="caption">
+        <h3>Interview <small><small><span class="label label-success">Version: Questions</span> <span class="label label-warning">In-Progress</span></small></small></h3>
+        <div><span class="label label-warning">Appendix</span> <span class="label label-primary">Due: September 5th 2014</span></div><br>
+		<div class="btn-group ">
+		  <a href="/p/appendix/interview.php" class="btn btn-primary" role="button">View</a>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="/p/appendix/interview.pdf">PDF</a></li>
+    			<li><a href="/p/appendix/interview.docx">Word Doc</a></li>
+        		<li><a href="/p/appendix/interview.txt">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/330x210/text:Feedback/#222222:#ffffff" alt="Feedback">
+      <div class="caption">
+        <h3>Feedback <small><small><span class="label label-danger">Version: N/A</span> <span class="label label-danger">On-Going</span></small></small></h3>
+        <div><span class="label label-warning">Appendix</span> <span class="label label-primary">Due: January 8th 2015</span></div><br>
+		<div class="btn-group ">
+		  <a href="/p/appendix/interview.php" class="btn btn-primary" role="button">View</a>
+ 		  <div class="btn-group">
+  		  	<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Download <span class="caret"></span></button>
+  			<ul class="dropdown-menu" role="menu">
+   	     		<li><a href="#">PDF</a></li>
+    			<li><a href="#">Word Doc</a></li>
+        		<li><a href="#">Rich Text</a></li>
+   		   </ul>
+ 		  </div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div>
+  <br>
+<table>
+<tr height=20>
+<td width=100></td>
+<td width=220></td>
+<td width=400></td>
+<td width=200></td>
+<tr>
+
+<tr>
+<td width=320 height=50 colspan=2 valign='top'><p><b>Directions to access product or any other additional information</b></p></td>
+<td width=400 height=70 bgcolor="gray" valign='top'><p> </p>If the product does not load, please go to the Product folder and click on index.htm (edit as required)</td>
+<td width=200></td>
+<tr>
+
+<tr>
+<td width=320 colspan=2><p><i>Links to documentation are below</i></p></td>
+<td width=400></td>
+<td width=200></td>
+<tr>
+
+
+<tr>
+<td width=320 colspan=2 height='70'><p><b>WORD COUNT</b></p></td>
+<td width=400><p><b>________ (add word count here)</b></p></td>
+<td width=200></td>
+<tr>
+</table>
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+<div class="jumbotron">
+	<div class="container">
+    <p>
+    	<div style="text-align: center;"><a href="http://clovisd.com"><img style="border: 0px solid ; width: 427px; height: 69px;" alt="CWD" src="http://clovisd.com/h/sig/mcdfulllight.png"></a><br>
+    	<a style="color: rgb(117, 117, 117);" href="http://clovisd.com" target="_blank">Clovis Delmotte</a><span style="color: rgb(117, 117, 117);"> | ©2014-2015 All Rights Reserved | </span><a style="color: rgb(117, 117, 117);" href="http://clovisd.com">clovisd.com</a>
+</p>
+
